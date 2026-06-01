053abfe4b41c234239aeea3b904c920866653f60
Mohamed Abdalah Fall <abdalahfall@gmail.com>
2026-06-01 15:10:38 +0000

    fix: suppression des options mongo obsolètes

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,13 +1,17 @@
 import { DataSource } from "typeorm";
-import { User } from "../entities/User.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+// Utilisation de l'URL de Render ou repli sur le localhost pour ton PC
+const MONGO_URL = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mon-api";
+
 export const AppDataSource = new DataSource({
     type: "mongodb",
-    url: process.env.MONGO_URL || "mongodb://localhost:27017/mon-projet",
+    url: MONGO_URL,
     synchronize: true,
     logging: true,
-    entities: [User],
+    entities: [
+        process.env.NODE_ENV === "production" ? "src/entities/*.js" : "src/entities/*.ts"
+    ],
 });
